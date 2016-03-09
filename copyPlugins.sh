e3e592a21332862afed7e41b110526c27efc58e2
Vincent Berthiaume <vberthiaume@gmail.com>
2016-03-09 16:40:39 -0500

    added vst3 in copyplugins.sh

--- copyPlugins.sh
+++ copyPlugins.sh
@@ -10,6 +10,10 @@ pushd ~/Library/Audio/Plug-Ins/VST/
 zip -r ~/Desktop/SpatGRIS$VERSION.zip ./SpatGRIS3.vst 
 popd
 
+pushd ~/Library/Audio/Plug-Ins/VST3/
+zip -r ~/Desktop/SpatGRIS$VERSION.zip ./SpatGRIS3.vst3 
+popd
+
 #zip -rj ~/Desktop/ZirkOSC.zip ~/Library/Audio/Plug-Ins/Components/ZirkOSC3.component ~/Library/Audio/Plug-Ins/VST/ZirkOSC3.vst 
 
 echo "Created zip file SpatGRIS$VERSION.zip"
