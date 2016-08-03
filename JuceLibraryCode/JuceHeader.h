7c1cc20b8d1c0703a6bea053493d74fff0b466da
Vincent Berthiaume <vberthiaume@gmail.com>
2016-08-03 11:26:21 -0400

    update version number

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -41,8 +41,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "SpatGRIS";
-    const char* const  versionString  = "0.1.2";
-    const int          versionNumber  = 0x102;
+    const char* const  versionString  = "0.1.3";
+    const int          versionNumber  = 0x103;
 }
 #endif
 
