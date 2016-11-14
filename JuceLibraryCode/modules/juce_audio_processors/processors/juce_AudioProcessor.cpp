3dad643e73dd062adccd8c4ba89b445d7119211c
Vincent Berthiaume <vberthiaume@gmail.com>
2016-11-14 14:14:00 -0500

    had to resave in introjucer

--- JuceLibraryCode/modules/juce_audio_processors/processors/juce_AudioProcessor.cpp
+++ JuceLibraryCode/modules/juce_audio_processors/processors/juce_AudioProcessor.cpp
@@ -300,7 +300,6 @@ String AudioProcessor::getParameterText (int index, int maximumStringLength)
 
 int AudioProcessor::getParameterNumSteps (int index)
 {
-    std::cout << "AudioProcessor::getParameterNumSteps\n";
     if (AudioProcessorParameter* p = managedParameters[index])
         return p->getNumSteps();
 
