974c42111f1497b93ab04845480853d7571d81e2
Vincent Berthiaume <vberthiaume@gmail.com>
2016-11-23 10:48:07 -0500

    remove vst3 from copyPlugin.sh

--- copyPlugins.sh
+++ copyPlugins.sh
@@ -10,9 +10,9 @@ pushd ~/Library/Audio/Plug-Ins/VST/
 zip -r ~/Desktop/SpatGRIS$VERSION.zip ./SpatGRIS.vst 
 popd
 
-pushd ~/Library/Audio/Plug-Ins/VST3/
-zip -r ~/Desktop/SpatGRIS$VERSION.zip ./SpatGRIS.vst3 
-popd
+#pushd ~/Library/Audio/Plug-Ins/VST3/
+#zip -r ~/Desktop/SpatGRIS$VERSION.zip ./SpatGRIS.vst3 
+#popd
 
 #zip -rj ~/Desktop/ZirkOSC.zip ~/Library/Audio/Plug-Ins/Components/ZirkOSC3.component ~/Library/Audio/Plug-Ins/VST/ZirkOSC3.vst 
 
