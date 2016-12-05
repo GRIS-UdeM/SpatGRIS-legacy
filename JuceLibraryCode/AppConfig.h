bcab66d7dd4ca0ce169b113f6812e8a0377563a0
Vincent Berthiaume <vberthiaume@gmail.com>
2016-12-05 16:05:13 -0500

    change version number

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -245,13 +245,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                0.1.10
+ #define JucePlugin_Version                0.1.11
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0x10a
+ #define JucePlugin_VersionCode            0x10b
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "0.1.10"
+ #define JucePlugin_VersionString          "0.1.11"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
